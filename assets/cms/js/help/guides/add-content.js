b472bd464dc6dcf4af953814ab048cfa5c3fa52e
Michele Locati <michele@locati.it>
2020-04-01 09:28:55 +0200

    Re-enable help tours
    # Conflicts:
    #	assets/cms/js/help/guides/add-content-edit-mode.js
    #	assets/cms/js/help/guides/add-content.js
    #	assets/cms/js/help/guides/add-page.js
    #	assets/cms/js/help/guides/change-content-edit-mode.js
    #	assets/cms/js/help/guides/change-content.js
    #	assets/cms/js/help/guides/dashboard.js
    #	assets/cms/js/help/guides/location-panel.js
    #	assets/cms/js/help/guides/personalize.js
    #	assets/cms/js/help/guides/toolbar.js

--- assets/cms/js/help/guides/add-content.js
+++ assets/cms/js/help/guides/add-content.js
@@ -1,32 +1,32 @@
-/* eslint-disable no-new, no-unused-vars, camelcase */
-/* global Tourist, ccmi18n_helpGuides, ConcreteHelpGuideManager */
+/* jshint unused:vars, undef:true, browser:true, jquery:true */
+/* global ccmi18n_helpGuides, ConcreteHelpGuideManager, Tourist */
 
-/*
-    var i18n = ccmi18n_helpGuides['add-content'];
-    var steps = [{
-        content: '<p><span class="h5">' + i18n[0].title + '</span><br/>' + i18n[0].text + '</p>',
-        highlightTarget: true,
-        closeButton: true,
-        nextButton: false,
-        target: $('[data-guide-toolbar-action=add-content]'),
-        my: 'top left',
-        at: 'bottom center'
-    }];
+;(function(global, $) {
+	var i18n = ccmi18n_helpGuides['add-content'];
+	var steps = [{
+		content: '<p><span class="h5">' + i18n[0].title + '</span><br/>' + i18n[0].text + '</p>',
+		highlightTarget: true,
+		closeButton: true,
+		nextButton: false,
+		target: $('[data-guide-toolbar-action=add-content]'),
+		my: 'top left',
+		at: 'bottom center'
+	}];
 
-    var tour = new Tourist.Tour({
-        steps: steps,
-        tipClass: 'Bootstrap',
-        tipOptions:{
-            showEffect: 'slidein'
-        }
-    });
-    tour.on('start', function() {
-        ConcreteHelpGuideManager.enterToolbarGuideMode();
-        ConcreteHelpGuideManager.launchGuideOnRefresh('add-content-edit-mode');
-    });
-    tour.on('stop', function() {
-        ConcreteHelpGuideManager.exitToolbarGuideMode();
-    });
+	var tour = new Tourist.Tour({
+		steps: steps,
+		tipClass: 'Bootstrap',
+		tipOptions:{
+			showEffect: 'slidein'
+		}
+	});
+	tour.on('start', function() {
+		ConcreteHelpGuideManager.enterToolbarGuideMode();
+		ConcreteHelpGuideManager.launchGuideOnRefresh('add-content-edit-mode');
+	});
+	tour.on('stop', function() {
+		ConcreteHelpGuideManager.exitToolbarGuideMode();
+	});
 
-    ConcreteHelpGuideManager.register('add-content', tour);
-    */
+	ConcreteHelpGuideManager.register('add-content', tour);
+})(window, jQuery);
--- assets/cms/js/help/guides/change-content.js
+++ assets/cms/js/help/guides/change-content.js
@@ -1,29 +1,32 @@
-/*
-    var i18n = ccmi18n_helpGuides['change-content'];
-    var steps = [{
-        content: '<p><span class="h5">' + i18n[0].title + '</span><br/>' + i18n[0].text + '</p>',
-        highlightTarget: false,
-        closeButton: true,
-        nextButton: false,
-        target: $('[data-guide-toolbar-action=edit-page]'),
-        my: 'top left',
-        at: 'bottom center'
-    }];
+/* jshint unused:vars, undef:true, browser:true, jquery:true */
+/* global ccmi18n_helpGuides, ConcreteHelpGuideManager, Tourist */
 
-    var tour = new Tourist.Tour({
-        steps: steps,
-        tipClass: 'Bootstrap',
-        tipOptions:{
-            showEffect: 'slidein'
-        }
-    });
-    tour.on('start', function() {
-        ConcreteHelpGuideManager.enterToolbarGuideMode();
-        ConcreteHelpGuideManager.launchGuideOnRefresh('change-content-edit-mode');
-    });
-    tour.on('stop', function() {
-        ConcreteHelpGuideManager.exitToolbarGuideMode();
-    });
+;(function(global, $) {
+	var i18n = ccmi18n_helpGuides['change-content'];
+	var steps = [{
+		content: '<p><span class="h5">' + i18n[0].title + '</span><br/>' + i18n[0].text + '</p>',
+		highlightTarget: false,
+		closeButton: true,
+		nextButton: false,
+		target: $('[data-guide-toolbar-action=edit-page]'),
+		my: 'top left',
+		at: 'bottom center'
+	}];
 
-    ConcreteHelpGuideManager.register('change-content', tour);
-*/
+	var tour = new Tourist.Tour({
+		steps: steps,
+		tipClass: 'Bootstrap',
+		tipOptions:{
+			showEffect: 'slidein'
+		}
+	});
+	tour.on('start', function() {
+		ConcreteHelpGuideManager.enterToolbarGuideMode();
+		ConcreteHelpGuideManager.launchGuideOnRefresh('change-content-edit-mode');
+	});
+	tour.on('stop', function() {
+		ConcreteHelpGuideManager.exitToolbarGuideMode();
+	});
+
+	ConcreteHelpGuideManager.register('change-content', tour);
+})(window, jQuery);
