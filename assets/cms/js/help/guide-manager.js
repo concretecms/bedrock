7d8a388d8d29415a3654b239aa900113d586eaed
Michele Locati <michele@locati.it>
2020-04-08 14:00:23 +0200

    Avoid isolation
    It's already when we generate the assets

--- assets/cms/js/help/guide-manager.js
+++ assets/cms/js/help/guide-manager.js
@@ -1,114 +1,108 @@
-/* jshint unused:vars, undef:true, browser:true, jquery:true */
-/* global Tour, CCM_REL */
+'use strict';
 
-;(function(global, $) {
-    'use strict';
+var KEY_GUIDETOLAUNCHONREFRESH = 'ConcreteHelpActiveGuide';
+var guides = {};
 
-    var KEY_GUIDETOLAUNCHONREFRESH = 'ConcreteHelpActiveGuide';
-    var guides = {};
+var ConcreteHelpGuideManager = {
+    register: function (key, guide) {
+        guides[key] = guide;
+    },
 
-    var ConcreteHelpGuideManager = {
-        register: function (key, guide) {
-            guides[key] = guide;
-        },
-
-        getGuide: function (key) {
-            var guide = guides[key];
-            if (!guide) {
-                return guide;
-            }
-            if (!(guide instanceof Tour)) {
-                guides[key] = guide = guide();
-            }
+    getGuide: function (key) {
+        var guide = guides[key];
+        if (!guide) {
             return guide;
-        },
+        }
+        if (!(guide instanceof Tour)) {
+            guides[key] = guide = guide();
+        }
+        return guide;
+    },
 
-        enterToolbarGuideMode: function() {
-            this.showOverlay();
-            this.raiseToolbar();
-        },
+    enterToolbarGuideMode: function() {
+        this.showOverlay();
+        this.raiseToolbar();
+    },
 
-        showOverlay: function() {
-            // if the widget overlay doesn't exist, show it
-            if ($('.ui-widget-overlay').length < 1) {
-                $('<div class="ui-widget-overlay"></div>').hide().appendTo('body');
-            }
-            $('.ui-widget-overlay').addClass('animated fadeIn').show();
+    showOverlay: function() {
+        // if the widget overlay doesn't exist, show it
+        if ($('.ui-widget-overlay').length < 1) {
+            $('<div class="ui-widget-overlay"></div>').hide().appendTo('body');
+        }
+        $('.ui-widget-overlay').addClass('animated fadeIn').show();
 
-        },
+    },
 
-        raiseToolbar: function() {
-            // move the toolbar to above the widget overlay
-            $('#ccm-toolbar').addClass('ccm-toolbar-tour-guide');
-        },
+    raiseToolbar: function() {
+        // move the toolbar to above the widget overlay
+        $('#ccm-toolbar').addClass('ccm-toolbar-tour-guide');
+    },
 
-        lowerToolbar: function() {
-            // move the toolbar back
-            $('#ccm-toolbar').removeClass('ccm-toolbar-tour-guide');
-        },
+    lowerToolbar: function() {
+        // move the toolbar back
+        $('#ccm-toolbar').removeClass('ccm-toolbar-tour-guide');
+    },
 
-        hideOverlay: function() {
-            $('.ui-widget-overlay').addClass('animated fadeOut');
-            $('.ui-widget-overlay').delay(250).queue(function() {
-                $(this).remove();
-                $(this).dequeue();
-            });
+    hideOverlay: function() {
+        $('.ui-widget-overlay').addClass('animated fadeOut');
+        $('.ui-widget-overlay').delay(250).queue(function() {
+            $(this).remove();
+            $(this).dequeue();
+        });
 
-        },
+    },
 
-        exitToolbarGuideMode: function() {
-            this.hideOverlay();
-            this.lowerToolbar();
-        },
+    exitToolbarGuideMode: function() {
+        this.hideOverlay();
+        this.lowerToolbar();
+    },
 
-        launchGuideOnRefresh: function(guide) {
-            window.localStorage.setItem(KEY_GUIDETOLAUNCHONREFRESH, guide);
-        },
+    launchGuideOnRefresh: function(guide) {
+        window.localStorage.setItem(KEY_GUIDETOLAUNCHONREFRESH, guide);
+    },
 
-        clearGuideToLaunchOnRefresh: function() {
-            window.localStorage.removeItem(KEY_GUIDETOLAUNCHONREFRESH);
-        },
+    clearGuideToLaunchOnRefresh: function() {
+        window.localStorage.removeItem(KEY_GUIDETOLAUNCHONREFRESH);
+    },
 
-        getGuideToLaunchOnRefresh: function() {
-            return window.localStorage.getItem(KEY_GUIDETOLAUNCHONREFRESH);
-        },
+    getGuideToLaunchOnRefresh: function() {
+        return window.localStorage.getItem(KEY_GUIDETOLAUNCHONREFRESH);
+    },
 
-        updateStepFooter: function (tour) {
-            var $tour = $('.ccm-help-tour'),
-                numSteps = tour.getStepCount();
-            if (numSteps > 1) {
-                $tour
-                    .find('.ccm-help-tour-position-index').text(1 + tour.getCurrentStepIndex()).end()
-                    .find('.ccm-help-tour-position-count').text(numSteps).end()
-                ;
-            } else {
-                $tour.find('.ccm-help-tour-footer').remove();
-            }
-        },
+    updateStepFooter: function (tour) {
+        var $tour = $('.ccm-help-tour'),
+            numSteps = tour.getStepCount();
+        if (numSteps > 1) {
+            $tour
+                .find('.ccm-help-tour-position-index').text(1 + tour.getCurrentStepIndex()).end()
+                .find('.ccm-help-tour-position-count').text(numSteps).end()
+            ;
+        } else {
+            $tour.find('.ccm-help-tour-footer').remove();
+        }
+    },
 
-        get: function() {
-            return ConcreteHelpGuideManager;
-        },
+    get: function() {
+        return ConcreteHelpGuideManager;
+    },
 
-        // Temporary fix for https://github.com/IGreatlyDislikeJavascript/bootstrap-tourist/issues/50
-        POSITIONING_BUG_HACK_ID: 'ccm-help-tour-hack',
+    // Temporary fix for https://github.com/IGreatlyDislikeJavascript/bootstrap-tourist/issues/50
+    POSITIONING_BUG_HACK_ID: 'ccm-help-tour-hack',
 
-        // Temporary fix for https://github.com/IGreatlyDislikeJavascript/bootstrap-tourist/issues/50
-        createPositioningBugHackElement: function($target) {
-            var $hack;
-            $hack = $('<div id="' + this.POSITIONING_BUG_HACK_ID + '" />').css({
-                position: 'absolute',
-                left: $target.offset().left,
-                top: $target.offset().top,
-                width: $target.width(),
-                height: $target.height(),
-            });
-            $(document.body).append($hack);
-            return $hack;
-        },
-    };
+    // Temporary fix for https://github.com/IGreatlyDislikeJavascript/bootstrap-tourist/issues/50
+    createPositioningBugHackElement: function($target) {
+        var $hack;
+        $hack = $('<div id="' + this.POSITIONING_BUG_HACK_ID + '" />').css({
+            position: 'absolute',
+            left: $target.offset().left,
+            top: $target.offset().top,
+            width: $target.width(),
+            height: $target.height(),
+        });
+        $(document.body).append($hack);
+        return $hack;
+    },
+};
 
 
-    global.ConcreteHelpGuideManager = ConcreteHelpGuideManager;
-
-})(window, jQuery);
+window.ConcreteHelpGuideManager = ConcreteHelpGuideManager;
