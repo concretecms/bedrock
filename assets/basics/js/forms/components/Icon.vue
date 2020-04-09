e1dbef5be1cc81319544b80897b79b972e2b38cd
Mike Esparza <mike@portlandlabs.com>
2020-04-09 09:54:18 -0700

    Initial icon implementation

---
+++ assets/basics/js/forms/components/Icon.vue
@@ -0,0 +1,14 @@
+<template functional>
+    <span>
+        <span class="icon"><i :class="[props.icon]"></i></span>
+        <span class="label"><slot /></span>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: String
+    }
+}
+</script>
