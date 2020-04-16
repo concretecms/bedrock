41e1bed25ecfc5e82f6507a61b69257bcc834c66
Korvin Szanto <korvin@portlandlabs.com>
2020-04-16 09:41:28 -0700

    Convert icon and iconbutton stories to use new constants, swap alerts for a click counter to keep dev flow smooth

--- stories/basics/icon.stories.js
+++ stories/basics/icon.stories.js
@@ -1,24 +1,54 @@
-import Icon from '../../assets/cms/components/Icon.vue'
+import Icon, { Icons, Types } from '../../assets/cms/components/Icon.vue'
 import Vue from 'vue'
 
 Vue.component('Icon', Icon)
 
 export default {
-    title: 'Components/Icon',
+    title: 'Basics/Icon',
     component: Icon
 }
 
 export const basicUsage = () => ({
+    data: () => ({
+        svg: Icons.svg,
+        fas: Icons.fas,
+        far: Icons.far,
+        fab: Icons.fab,
+        types: Object.values(Types).filter(type => type !== 'svg')
+    }),
     template: `
-        <div>
-            <p>With fontawesome solid icons</p>
-            <Icon icon="fa-trash" icon-type="fontawesome-solid" icon-color="#7F7F7F"/>    
-            
-            <p>With fontawesome regular icons</p>
-            <Icon icon="fa-trash-alt" icon-type="fontawesome-regular" icon-color="#7F7F7F"/>    
-            
-            <p>With fontawesome brand icons</p>
-            <Icon icon="fa-vuejs" icon-type="fontawesome-brand" icon-color="#41b883"/>
-        </div>
+    <div class='ccm-ui'>
+        <fieldset >
+            <legend>SVGs</legend>
+            <div v-for='icon in svg' style='display:inline-block;width:50px;height:50px;' :title='icon'>
+                <Icon :icon='icon' type="${Types.svg}" color="blue" />
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Fontawesome</legend>
+            <div class='d-flex'>
+                <div style='flex-grow: 1; flex-basis: 0;'>
+                    <p>With fontawesome solid icons</p>
+                    <div v-for='icon of fas' style='display:inline-block;width:50px;height:50px;' :title='icon'>
+                        <Icon :icon='icon' type="${Types.fas}" color="#7F7F7F" />
+                    </div>
+                </div>
+
+                <div style='flex-grow: 1; flex-basis: 0;'>
+                    <p>With fontawesome regular icons</p>
+                    <div v-for='icon of far' style='display:inline-block;width:50px;height:50px;' :title='icon'>
+                        <Icon :icon='icon' type="${Types.far}" color="red" />
+                    </div>
+                </div>
+
+                <div style='flex-grow: 1; flex-basis: 0;'>
+                    <p>With fontawesome brand icons</p>
+                    <div v-for='icon of fab' style='display:inline-block;width:50px;height:50px;' :title='icon'>
+                        <Icon :icon='icon' type="${Types.fab}" color="#41b883" />
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+    </div>
     `
 })
--- stories/basics/iconButton.stories.js
+++ stories/basics/iconButton.stories.js
@@ -1,50 +1,61 @@
 import IconButton from '../../assets/cms/components/IconButton.vue'
+import { Types, Icons } from '../../assets/cms/components/Icon.vue'
 import Vue from 'vue'
 
 Vue.component('IconButton', IconButton)
 
 export default {
-    title: 'Components/IconButton',
+    title: 'Basics/IconButton',
     component: IconButton
 }
 
-export const basicUsage = () => ({
+export const iconButton = () => ({
     component: {
         IconButton
     },
-    methods: {
-        handleClick() {
-            alert('Button Clicked.')
-        }
-    },
+    data: () => ({
+        clicks: 0
+    }),
     template: `
     <div class="ccm-ui">
-        <IconButton
-            icon="fa-trash-alt"
-            icon-type="fontawesome-regular"
-            icon-color="#7F7F7F"
-            @click="handleClick"
-        />  
+        <p>Clicks: {{clicks}}</p>
+        <IconButton icon="${Icons.far.trashAlt}" @click="clicks++" />
     </div>
     `
 })
 
-export const basicUsageWithLabel = () => ({
+console.log(Icons)
+export const svgUsage = () => ({
     component: {
         IconButton
     },
-    methods: {
-        handleClick() {
-            alert('Button Clicked.')
-        }
-    },
+    data: () => ({
+        clicks: 0
+    }),
     template: `
     <div class="ccm-ui">
+        <p>Clicks: {{clicks}}</p>
+        <IconButton iconType="${Types.svg}" icon="${Icons.svg.plus}" @click="clicks++" />
+        <IconButton iconType="${Types.svg}" icon="${Icons.svg.dialogClose}" @click="clicks++" />
+    </div>
+    `
+})
+
+export const labeledButton = () => ({
+    component: {
+        IconButton
+    },
+    data: () => ({
+        clicks: 0
+    }),
+    template: `
+    <div class="ccm-ui">
+        <p>Clicks: {{clicks}}</p>
         <IconButton
-            icon="fa-trash-alt"
-            icon-type="fontawesome-regular"
+            icon="${Icons.far.trashAlt}"
+            icon-type="${Types.far}"
             icon-color="#7F7F7F"
-            @click="handleClick"
+            @click="clicks++"
         >
             Delete
         </IconButton>  
@@ -53,21 +64,20 @@ export const basicUsageWithLabel = () => ({
     `
 })
 
-export const withOutlineIconOnly = () => ({
+export const iconButtonWithOutline = () => ({
     component: {
         IconButton
     },
-    methods: {
-        handleClick() {
-            alert('Button Clicked.')
-        }
-    },
+    data: () => ({
+        clicks: 0
+    }),
     template: `
     <div class="ccm-ui">
+        <p>Clicks: {{clicks}}</p>
         <IconButton
-            icon="fa-trash-alt"
-            icon-type="fontawesome-regular"
-            @click="handleClick"
+            icon="${Icons.far.trashAlt}"
+            icon-type="${Types.far}"
+            @click="clicks++"
             format="outline-primary"
         />  
     </div>
@@ -75,21 +85,20 @@ export const withOutlineIconOnly = () => ({
     `
 })
 
-export const withLabelOutline = () => ({
+export const labeledWithOutline = () => ({
     component: {
         IconButton
     },
-    methods: {
-        handleClick() {
-            alert('Button Clicked.')
-        }
-    },
+    data: () => ({
+        clicks: 0
+    }),
     template: `
     <div class="ccm-ui">
+        <p>Clicks: {{clicks}}</p>
         <IconButton
-            icon="fa-trash-alt"
-            icon-type="fontawesome-regular"
-            @click="handleClick"
+            icon="${Icons.far.trashAlt}"
+            icon-type="${Types.far}"
+            @click="clicks++"
             format="outline"
             label-position="left"
         >
@@ -100,21 +109,20 @@ export const withLabelOutline = () => ({
     `
 })
 
-export const withLabelOutlineDisabled = () => ({
+export const disabled = () => ({
     component: {
         IconButton
     },
-    methods: {
-        handleClick() {
-            alert('Button Clicked.')
-        }
-    },
+    data: () => ({
+        clicks: 0
+    }),
     template: `
     <div class="ccm-ui">
+        <p>Clicks: {{clicks}}</p>
         <IconButton
-            icon="fa-trash-alt"
-            icon-type="fontawesome-regular"
-            @click="handleClick"
+            icon="${Icons.far.trashAlt}"
+            icon-type="${Types.far}"
+            @click="clicks++"
             format="outline"
             disabled  
         >
