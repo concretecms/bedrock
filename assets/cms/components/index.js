a66bdfd4cccdd8b9f68288d6748f5fde55d65259
Korvin Szanto <korvin@portlandlabs.com>
2020-05-15 09:47:42 -0700

    Fix lint for components index

--- assets/cms/components/index.js
+++ assets/cms/components/index.js
@@ -1,18 +1,18 @@
-import Toggle from "./form/Toggle"
-import ContextMenu from "./menu/ContextMenu"
-import MenuButton from "./menu/MenuButton"
-import MenuDivider from "./menu/MenuDivider"
-import MenuItem from "./menu/MenuItem"
-import Icon from "./Icon"
-import IconButton from "./IconButton"
+import Toggle from './form/Toggle'
+import ContextMenu from './menu/ContextMenu'
+import MenuButton from './menu/MenuButton'
+import MenuDivider from './menu/MenuDivider'
+import MenuItem from './menu/MenuItem'
+import Icon from './Icon'
+import IconButton from './IconButton'
 
 // Export our component library
 export default {
-  Toggle,
-  ContextMenu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  Icon,
-  IconButton,
+    Toggle,
+    ContextMenu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    Icon,
+    IconButton
 }
