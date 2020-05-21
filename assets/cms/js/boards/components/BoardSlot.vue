b2e312a4bf2a86fbb69e4098dcb18479d90ed987
Andrew Embler <andrew@concrete5.org>
2020-05-21 07:00:27 -0700

    renaming variable for fixes

--- assets/cms/js/boards/components/BoardSlot.vue
+++ assets/cms/js/boards/components/BoardSlot.vue
@@ -13,7 +13,7 @@ import '../../in-context-menu'
 
 export default {
     props: {
-        slot: Object
+        slotData: Object
     },
     data: () => ({
         menu: null,
@@ -37,22 +37,22 @@ export default {
         }
     },
     mounted() {
-        this.isPinned = this.slot.isPinned
+        this.isPinned = this.slotData.isPinned
         var my = this
         /* eslint-disable-next-line no-unused-vars */
         const menu = new ConcreteMenu($(this.$el), {
             highlightClassName: 'ccm-block-highlight',
             menuActiveClass: 'ccm-block-highlight',
-            menu: 'div[data-menu-board-instance-slot-id=' + my.slot.slot + ']'
+            menu: 'div[data-menu-board-instance-slot-id=' + my.slotData.slot + ']'
         })
 
         menu.$menu.find('a[data-menu-action=pin-item]').on('click', function () {
             new ConcreteAjaxRequest({
                 url: CCM_DISPATCHER_FILENAME + '/ccm/system/board/instance/pin_slot',
                 data: {
-                    slot: my.slot.slot,
-                    boardInstanceID: my.slot.boardInstanceID,
-                    bID: my.slot.bID,
+                    slot: my.slotData.slot,
+                    boardInstanceID: my.slotData.boardInstanceID,
+                    bID: my.slotData.bID,
                     action: 'pin'
                 },
                 success: function (r) {
@@ -65,8 +65,8 @@ export default {
             new ConcreteAjaxRequest({
                 url: CCM_DISPATCHER_FILENAME + '/ccm/system/board/instance/pin_slot',
                 data: {
-                    slot: my.slot.slot,
-                    boardInstanceID: my.slot.boardInstanceID,
+                    slot: my.slotData.slot,
+                    boardInstanceID: my.slotData.boardInstanceID,
                     action: 'unpin'
                 },
                 success: function (r) {
