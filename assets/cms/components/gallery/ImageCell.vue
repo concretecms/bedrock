d045f682c64934faf139564e199bcc1c0d1f7953
Mike Esparza <mike@portlandlabs.com>
2020-06-19 15:05:06 -0700

    Moving styling out the component so can be re-usable glabally

--- assets/cms/components/gallery/ImageCell.vue
+++ assets/cms/components/gallery/ImageCell.vue
@@ -1,69 +1,16 @@
 <template functional>
-    <div class="ccm-image-cell text-center" :class="{ active: props.isActive }">
-        <button type="button" class="delete" @click="listeners.delete">
-            <Icon icon="times" type="fas" color="#fff"/>
-        </button>
-        <div @click="listeners.click">
-            <img :src="props.src" :style="{ width: props.size + 'px', height: props.size + 'px' }" />
-            <p>{{ props.fileSize }}</p>
-        </div>
+    <div class="ccm-image-cell-grid">
+      <div class="ccm-image-cell v-component text-center" :class="{ active: props.isActive }">
+          <button type="button" class="delete" @click="listeners.delete">
+              <Icon icon="times" type="fas" color="#fff"/>
+          </button>
+          <div @click="listeners.click">
+              <img :src="props.src" :style="{ width: props.size + 'px', height: props.size + 'px' }" />
+              <p>{{ props.fileSize }}</p>
+          </div>
+      </div>
     </div>
 </template>
-
-<style lang="scss" scoped>
-.ccm-image-cell {
-  cursor: pointer;
-  display: inline-flex;
-  flex-direction: column;
-  margin: 10px;
-  position: relative;
-
-  &:hover {
-    .delete {
-      opacity: 1;
-      transition: opacity 0.2s ease-out;
-    }
-  }
-
-  &:hover,
-  &.active {
-    img {
-      border: 2px solid #4a90e2;
-      opacity: 1;
-    }
-  }
-
-  p {
-    color: #676b74;
-    font-size: 1rem;
-    letter-spacing: 0;
-    margin-top: 0.625rem;
-    text-transform: uppercase;
-  }
-
-  img {
-    border: 2px solid #979797;
-    box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
-    opacity: 0.7;
-    padding: 0.5rem;
-    transition: opacity 0.5s ease-out;
-  }
-
-  .delete {
-    background-color: #6a6f7b;
-    border-radius: 20px;
-    float: left;
-    height: 30px;
-    margin-right: -10px;
-    margin-top: -10px;
-    opacity: 0;
-    position: absolute;
-    right: 0;
-    width: 30px;
-    z-index: 90;
-  }
-}
-</style>
 
 <script>
 import Icon from '../Icon'
