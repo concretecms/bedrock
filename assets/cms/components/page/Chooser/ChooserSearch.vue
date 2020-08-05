c5c23fd4763f3c70ded6a3bf616a124aaa5242a6
Ruud Walraven <ruudwalraven@zuiderlicht.nl>
2020-08-05 23:16:30 +0200

    Update front end page search to include paging and sorting

--- assets/cms/components/page/Chooser/ChooserSearch.vue
+++ assets/cms/components/page/Chooser/ChooserSearch.vue
@@ -3,51 +3,53 @@
         <div class="form-inline mt-3">
             <div class="form-group ml-auto">
                 <label class="mr-2">Search</label>
-                <input class="form-control" v-model="keyword"/>
+                <input class="form-control" v-model="keywords"/>
             </div>
         </div>
-        <div v-show="!keyword" class="text-center mt-5">
+        <div v-show="!keywords" class="text-center mt-5">
             <span class="search-icon my-4">
                 <Icon icon="search" type="fas" color="#f4f4f4"/>
             </span>
             <p><b>Let's get some info on what you're looking for.</b></p>
         </div>
-        <div>
-            <PageList v-if="this.keyword"
-                      :routePath="this.routePath + this.keyword"
-                      @click="page => choosePage(page)"/>
+        <div v-if="keywords">
+            <PageList
+                :keywords="keywords"
+                :routePath="routePath"
+                @click="page => choosePage(page)"/>
+
         </div>
     </div>
 </template>
 
 <script>
-import Icon from '../../Icon'
-import PageList from './../PageList'
+    import Icon from '../../Icon';
+    import PageList from './../PageList';
 
-export default {
-    components: { PageList, Icon },
-    data: () => ({
-        keyword: '',
-        routePath: '/ccm/system/page/chooser/search/'
-    }),
-    methods: {
-        choosePage (page) {
-            window.ConcreteEvent.publish('SitemapSelectPage', {
-                instance: this,
-                cID: page.cID,
-                title: page.name
-            })
+    export default {
+        components: {Icon, PageList},
+        data: () => ({
+            keywords: '',
+            routePath: '/ccm/system/page/chooser/search/'
+        }),
+        methods: {
+            choosePage (page) {
+                window.ConcreteEvent.publish('SitemapSelectPage', {
+                    instance: this,
+                    cID: page.cID,
+                    title: page.name
+                });
+            }
         }
-    }
-}
+    };
 </script>
 
 <style lang="scss" scoped>
-    .search-icon {
-        display: inline-block;
+  .search-icon {
+    display: inline-block;
 
-        .icon {
-            font-size: 7rem;
-        }
+    .icon {
+      font-size: 7rem;
     }
+  }
 </style>
--- assets/cms/components/page/PageList.vue
+++ assets/cms/components/page/PageList.vue
@@ -3,45 +3,70 @@
         <svg v-if="isLoading" class="ccm-loader-dots">
             <use xlink:href="#icon-loader-circles"/>
         </svg>
-        <table v-else class="table ccm-page-list-view ccm-search-results-table">
-            <thead>
-            <tr>
-                <th>Type</th>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Last Modified</th>
-                <th>Author</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="page in pageList" :key="page.cID + 'list'"
-                @click="choosePage(page)"
-                @mouseover="mouseOver = page.cID"
-                @mouseout="mouseOver = 0"
-                :class="{'ccm-search-select-hover': mouseOver === page.cID}">
-                <td>{{page.type}}</td>
-                <td>{{page.name}}</td>
-                <td>{{page.datePublic}}</td>
-                <td>{{page.dateModified}}</td>
-                <td>{{page.author}}</td>
-            </tr>
-            </tbody>
-        </table>
+        <div v-else>
+            <table class="table ccm-page-list-view ccm-search-results-table">
+                <thead>
+                <tr>
+                    <th><span>Type</span></th>
+                    <th :class="orderBy === 'cv.cvName' ? 'ccm-results-list-active-sort-' + orderByDirection : ''"><a href="#" @click="sortResults('cv.cvName', 'asc', $event)">Name</a></th>
+                    <th :class="orderBy === 'cv.cvDatePublic' ? 'ccm-results-list-active-sort-' + orderByDirection : ''"><a href="#" @click="sortResults('cv.cvDatePublic', 'asc', $event)">Date</a></th>
+                    <th :class="orderBy === 'c.cDateModified' ? 'ccm-results-list-active-sort-' + orderByDirection : ''"><a href="#" @click="sortResults('c.cDateModified', 'desc', $event)">Last Modified</a></th>
+                    <th><span>Author</span></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="page in pageList" :key="page.cID + 'list'"
+                    @click="choosePage(page)"
+                    @mouseover="mouseOver = page.cID"
+                    @mouseout="mouseOver = 0"
+                    :class="{'ccm-search-select-hover': mouseOver === page.cID}">
+                    <td>{{page.type}}</td>
+                    <td>{{page.name}}</td>
+                    <td>{{page.datePublic}}</td>
+                    <td>{{page.dateModified}}</td>
+                    <td>{{page.author}}</td>
+                </tr>
+                </tbody>
+            </table>
+            <BootstrapPagination
+                v-if="pagination.total_pages > 1"
+                v-model="currentPage"
+                :total-rows="pagination.total"
+                :per-page="pagination.per_page"></BootstrapPagination>
+        </div>
     </div>
 </template>
 
 <script>
+import BootstrapPagination from '../BootstrapPagination'
+
 /**
  * @vue-data {false|Array<Object>} pageList - Pages to display. When not yet initialised this is set to false.
  * @vue-data {Number} mouseOver - Contains cID of the page row that the mouse hovers over for adding a hover class name.
+ * @vue-prop {String} [keywords] - Optional search parameter
  * @vue-prop {String} routePath - Origin for pageList data which is fetched by ConcreteAjaxRequest.
  */
 export default {
+    components: { BootstrapPagination },
     data: () => ({
+        mouseOver: 0,
+        orderBy: 'c.cDateModified',
+        orderByDirection: 'desc',
+        currentPage: 1,
         pageList: false,
-        mouseOver: 0
+        pagination: {
+            total: 0,
+            count: 0,
+            per_page: 2,
+            current_page: 1,
+            total_pages: 0
+        }
     }),
     props: {
+        keywords: {
+            type: String,
+            required: true
+        },
         routePath: {
             type: String,
             required: true
@@ -54,14 +79,48 @@ export default {
     },
     methods: {
         /**
+         * Sort search results by sortField
+         * @param {String} sortField - Order by this field
+         * @param {String} initialSortOrder - If the field wasn't sorted on yet, then use the initial order
+         * @param {Event} event - The click event
+         */
+        sortResults (sortField, initialSortOrder, event) {
+            if (this.orderBy === sortField) {
+                this.orderByDirection = this.orderByDirection === 'desc' ? 'asc' : 'desc'
+            } else {
+                this.orderByDirection = initialSortOrder
+            }
+
+            this.orderBy = sortField
+
+            // Cancel href click
+            event.stopPropagation()
+        },
+
+        /**
          * Retrieves pages from the routePath url. On success this updates
          * this.pageList. That triggers Vue to rerender search result
          * @returns ConcreteAjaxRequest - The request that performs the fetch and updates this.pageList
          */
         fetchPages () {
             return new ConcreteAjaxRequest({
-                url: CCM_DISPATCHER_FILENAME + this.$props.routePath,
+                url: CCM_DISPATCHER_FILENAME + this.$props.routePath + this.keywords +
+                        '?ccm_order_by=' + this.orderBy +
+                        '&ccm_order_by_direction=' + this.orderByDirection +
+                        '&ccm_paging_p=' + this.currentPage +
+                        '&itemsPerPage=' + this.pagination.per_page,
                 success: response => {
+                    // Check the results match the highlighted page number (this might be a delayed result)
+                    if (response.meta && response.meta.pagination && response.meta.pagination.current_page !== this.currentPage) {
+                        return
+                    }
+
+                    // Update pagination
+                    if (response.meta && response.meta.pagination) {
+                        this.pagination = response.meta.pagination
+                    }
+
+                    // Update page results
                     if (response.data.length) {
                         this.pageList = response.data
                     }
@@ -79,7 +138,19 @@ export default {
         }
     },
     watch: {
-        routePath: () => {
+        currentPage () {
+            this.fetchPages()
+        },
+
+        orderBy () {
+            this.fetchPages()
+        },
+
+        orderByDirection () {
+            this.fetchPages()
+        },
+
+        routePath () {
             this.fetchPages()
         }
     },
