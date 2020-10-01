1cecde2fe13936801526f9d4075a5dcaf0d4aaf5
Michele Locati <michele@locati.it>
2020-10-01 10:38:17 +0200

    Root tools to routes: reindex_pending_pages

--- assets/cms/js/page-indexer.js
+++ assets/cms/js/page-indexer.js
@@ -1,7 +1,7 @@
 var ConcretePageIndexer = {
 
     reindexPendingPages: function () {
-        $.get(CCM_TOOLS_PATH + '/reindex_pending_pages?ccm_token=' + CCM_SECURITY_TOKEN)
+        $.get(CCM_DISPATCHER_FILENAME + '/ccm/page/reindex_pending?ccm_token=' + CCM_SECURITY_TOKEN)
     }
 
 }
