5936afb24f6445e49940247d4f02e60fddeaa819
Andrew Embler <andrew@concrete5.org>
2020-10-14 12:12:37 -0700

    making uploads work

--- assets/cms/components/file-manager/Uploader/UploadFromComputer.vue
+++ assets/cms/components/file-manager/Uploader/UploadFromComputer.vue
@@ -45,11 +45,22 @@ export default {
     props: {
         uploadDirectoryId: {
             type: Number,
-            required: true
+            required: false,
+            default: null
+        },
+        maxFiles: {
+            type: Number,
+            required: false,
+            default: null
         },
         options: {
             type: Object,
             default: () => ({})
+        },
+        replaceFileId: {
+            type: Number,
+            required: false,
+            default: null
         }
     },
     computed: {
@@ -68,7 +79,7 @@ export default {
                 uploadMultiple: true,
                 parallelUploads: 4,
                 paramName: 'files',
-                maxFiles: null,
+                maxFiles: this.maxFiles,
                 autoQueue: true,
 
                 addedfiles: function () {
@@ -104,15 +115,7 @@ export default {
                     formData.append('responseFormat', 'dropzone')
                     formData.append('ccm_token', CCM_SECURITY_TOKEN)
                     formData.append('currentFolder', me.uploadDirectoryId)
-
-                    if (typeof me.options.formData === 'object') {
-                        let key = ''
-
-                        for (key in me.options.formData) {
-                            // noinspection JSUnfilteredForInLoop
-                            formData.append(key, me.options.formData[key])
-                        }
-                    }
+                    formData.append('fID', me.replaceFileId)
                 },
 
                 totaluploadprogress: function (progress) {
@@ -122,17 +125,7 @@ export default {
                 queuecomplete: function () {
                     if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
                         if (me.uploadedFiles.length !== 0) {
-                            /*
-                            if (typeof me.options.formData.fID === 'undefined') {
-                                ConcreteEvent.publish('FileManagerAddFilesComplete', {
-                                    files: me.uploadedFiles
-                                })
-                            } else {
-                                ConcreteEvent.publish('FileManagerReplaceFileComplete', {
-                                    files: me.uploadedFiles
-                                })
-                            }
-                             */
+
                             const fileIds = [];
                             me.uploadedFiles.forEach(function(file) {
                                 fileIds.push(file.fID)
@@ -173,9 +166,9 @@ export default {
     },
     mounted() {
         const me = this
-        me.options.formData = me.options.formData || {}
+        const settings = me.dropzoneSettings
 
-        $(me.$refs.dropzoneElement).dropzone(me.dropzoneSettings)
+        $(me.$refs.dropzoneElement).dropzone(settings)
         $(window).on('resize', _.throttle(me.refresh, 100))
 
         ConcreteEvent.subscribe('FileUploaderUploadSelectedFiles', function(e) {
@@ -217,12 +210,6 @@ export default {
             thumbnailWidth -= 42 // padding + border (see css declaration of .ccm-file-upload-item-wrapper)
 
             $container.find('.ccm-file-upload-item-wrapper').css('width', `${thumbnailWidth}px`)
-
-            if (typeof this.options.formData.fID !== 'undefined') {
-                this.dropzone.options.maxFiles = 1
-            } else {
-                this.dropzone.options.maxFiles = null
-            }
         },
         reset() {
             this.isUploadInProgress = false
