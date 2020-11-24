331cf08c8d3689e6ad37a654f8f747e93c1c6ea9
Andrew Embler <andrew@concrete5.org>
2020-11-23 18:21:57 -0800

    fix lint

--- assets/cms/js/edit-mode/style-customizer/inline-toolbar.js
+++ assets/cms/js/edit-mode/style-customizer/inline-toolbar.js
@@ -104,8 +104,8 @@ ConcreteInlineStyleCustomizer.prototype = {
     setupSelectBoxes: function() {
         var my = this
 
-        my.$toolbar.find('.selectpicker').selectpicker();
-        
+        my.$toolbar.find('.selectpicker').selectpicker()
+
         var $customClass = my.$toolbar.find('#customClass')
 
         $customClass.selectpicker({
