72ba325e40f455bf8d1e492d7164922d3fc8ea26
Andrew Embler <andrew@concrete5.org>
2020-12-15 08:48:35 -0800

    task process list update:

--- assets/cms/components/TaskProcessList.vue
+++ assets/cms/components/TaskProcessList.vue
@@ -13,47 +13,58 @@
                 </div>
             </div>
         </div>
-        <div :class="{'card': true, 'card-body': true, 'process-card': true, 'process-card-expandable': detailsAction != '' && process.hasDetails}"
-        v-for="process in processes" :key="process.id"
-             @click="detailsAction != '' && process.hasDetails ? toggleProcess(process) : null">
-            <div class="row">
-                <div class="col-md-4">
-                    <div>
-                        <span v-if="detailsAction != '' && process.hasDetails" class="d-inline-block" style="width: 20px">
-                            <icon :icon="openProcesses.includes(process.id) ? 'chevron-down' : 'chevron-right'"></icon>
-                        </span>
-                        {{process.name}}
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="text-muted">{{process.dateStartedString}}</div>
-                </div>
-                <div class="col-md-3">
-                    <div class="text-muted">{{process.dateCompletedString}}</div>
-                </div>
-                <div class="col-md-2 d-flex">
-                    <div class="ml-auto">
-                        <a v-if="deleteAction != ''" href="#" class="ccm-hover-icon" @click.stop="deleteProcess(process)">
-                            <icon icon="trash"></icon>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div :id="'process-' + process.id" v-if="openProcesses.includes(process.id)">
-                <div class="pt-2 pr-4 pb-2 pl-4">
-                    <div v-if="process.details">
-                        <div v-for="detail in process.details">
-                            {{detail}}
+        <transition-group tag="div" class="process-card-wrapper" name="process-card-animation">
+            <div :class="{'card': true, 'card-body': true, 'process-card': true, 'process-card-expandable': detailsAction != '' && process.hasDetails}"
+            v-for="process in processes" :key="process.id"
+                 @click="detailsAction != '' && process.hasDetails ? toggleProcess(process) : null">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div>
+                            <span v-if="detailsAction != '' && process.hasDetails" class="d-inline-block" style="width: 20px">
+                                <icon :icon="openProcesses.includes(process.id) ? 'chevron-down' : 'chevron-right'"></icon>
+                            </span>
+                            {{process.name}}
                         </div>
                     </div>
-                    <span v-else class="text-muted">Loading...</span>
+                    <div class="col-md-3">
+                        <div class="text-muted">{{process.dateStartedString}}</div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="text-muted">{{process.dateCompletedString}}</div>
+                    </div>
+                    <div class="col-md-2 d-flex">
+                        <div class="ml-auto">
+                            <a v-if="deleteAction != ''" href="#" class="ccm-hover-icon" @click.stop="deleteProcess(process)">
+                                <icon icon="trash"></icon>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row process-progress-wrapper" v-show="process.batch !== null && process.dateCompleted === null">
+                    <div class="col-md-12">
+                        <div class="mt-2 ml-4 progress process-progress">
+                            <div class="progress-bar" role="progressbar" :style="'width: ' + process.progress + '%'"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div :id="'process-' + process.id" v-if="openProcesses.includes(process.id)">
+                    <div class="pt-2 pr-4 pb-2 pl-4">
+                        <div v-if="process.details">
+                            <div v-for="detail in process.details">
+                                {{detail}}
+                            </div>
+                        </div>
+                        <span v-else class="text-muted">Loading...</span>
+                    </div>
                 </div>
             </div>
-
-            <div v-if="deleteAction != ''" class="modal fade" tabindex="-1" role="dialog" :id="'delete-process-' + process.id">
+        </transition-group>
+        <div v-if="deleteAction !== ''">
+            <div v-for="process in processes" class="modal fade" tabindex="-1" role="dialog" :id="'delete-process-' + process.id">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
-                        <form method="post" :action="deleteAction">
+                        <form method="post" @submit.prevent="deleteProcessSubmit(process.id)">
                             <div class="modal-header">
                                 <h5 class="modal-title">Delete Process</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -61,7 +72,6 @@
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <input type="hidden" name="processId" :value="process.id">
                                 Delete this process log entry? The record of the process along with any logs will be removed.
                             </div>
                             <div class="modal-footer">
@@ -73,7 +83,10 @@
                 </div>
             </div>
         </div>
+
+
     </div>
+
 </template>
 
 <script>
@@ -86,7 +99,7 @@ export default {
         eventSource: {
             type: String,
             required: false,
-            default: false,
+            default: null
         },
         currentProcessId: {
             type: String,
@@ -109,26 +122,9 @@ export default {
         }
     },
     data: () => ({
-        'openProcesses': []
+        'openProcesses': [],
+        'subscribedProcesses': []
     }),
-    mounted() {
-        if (this.eventSource) {
-            const url = new URL(this.eventSource)
-            const my = this
-            url.searchParams.append('topic', 'https://global.concretecms.com/task/processes/{processId}')
-            const eventSource = new EventSource(url)
-            eventSource.onmessage = event => {
-                var data = JSON.parse(event.data)
-                if (data.processId) {
-                    my.processes.forEach(function(process) {
-                        if (process.id === data.processId) {
-                            process.details.push(data.message)
-                        }
-                    })
-                }
-            }
-        }
-    },
     methods: {
         deleteProcess(process) {
             var modalTarget = '#delete-process-' + process.id
@@ -156,9 +152,85 @@ export default {
             } else {
                 this.closeProcess(process)
             }
+        },
+        deleteProcessSubmit(processId) {
+            var my = this
+            new ConcreteAjaxRequest({
+                url: this.deleteAction,
+                data: {
+                    processId: processId,
+                },
+                success: function (r) {
+                    var modalTarget = '#delete-process-' + processId
+                    $(modalTarget).modal('hide')
+                    my.processes.forEach(function(process, i) {
+                        if (process.id == processId) {
+                            my.closeProcess(processId)
+                            my.processes.splice(i, 1)
+                        }
+                    })
+                }
+            })
         }
     },
     watch: {
+        processes: {
+            immediate: true,
+            handler: function(processes) {
+                const my = this
+                if (my.eventSource) {
+                    processes.forEach(function(process) {
+                        if (!my.subscribedProcesses.includes(process.id)) {
+                            var messageUrl = new URL(my.eventSource)
+                            messageUrl.searchParams.append('topic', 'https://global.concretecms.com/task/processes/' + process.id)
+                            var messageEventSource = new EventSource(messageUrl)
+                            messageEventSource.onmessage = event => {
+                                var data = JSON.parse(event.data)
+                                if (data.processId) {
+                                    my.processes.forEach(function (process) {
+                                        if (process.id === data.processId) {
+                                            process.details.push(data.message)
+                                        }
+                                    })
+                                }
+                            }
+
+                            var closeUrl = new URL(my.eventSource)
+                            closeUrl.searchParams.append('topic', 'https://global.concretecms.com/task/close-process/' + process.id)
+                            var closeEventSource = new EventSource(closeUrl)
+                            closeEventSource.onmessage = event => {
+                                var data = JSON.parse(event.data)
+                                my.processes.forEach(function (process) {
+                                    if (process.id == data.process.id) {
+                                        process.dateCompleted = data.process.dateCompleted
+                                        process.dateCompletedString = data.process.dateCompletedString
+                                    }
+                                })
+                            }
+
+                            if (process.batch) {
+                                var progressUrl = new URL(my.eventSource)
+                                progressUrl.searchParams.append('topic', 'https://global.concretecms.com/batches/' + process.batch.id)
+                                var progressEventSource = new EventSource(progressUrl)
+                                progressEventSource.onmessage = event => {
+                                    var data = JSON.parse(event.data)
+                                    var total = data.batch.totalJobs
+                                    var progress = total - data.batch.pendingJobs
+                                    var percent = Math.round(progress / total * 100)
+
+                                    my.processes.forEach(function (process) {
+                                        if (process.batch && process.batch.id == data.batch.id) {
+                                            process.progress = percent
+                                        }
+                                    })
+                                }
+                            }
+                            my.subscribedProcesses.push(process.id)
+                        }
+                    })
+                }
+            }
+        },
         currentProcessId: {
             immediate: true,
             handler: function(processId) {
@@ -178,6 +250,10 @@ export default {
 @import '~bootstrap/scss/functions';
 @import '~bootstrap/scss/variables';
 
+div.process-progress {
+    height: 2px;
+}
+
 div.process-card {
     margin-bottom: 1rem;
     padding: 0.75rem;
@@ -188,6 +264,37 @@ div.process-card-expandable {
     cursor: pointer;
     &:hover {
         border: 1px solid $blue;
+    }
+}
+
+.process-card-animation {
+    backface-visibility: hidden;
+    z-index: 1;
+}
+
+/* moving */
+.process-card-animation-move {
+    transition: all 600ms ease-in-out 50ms;
+}
+
+/* appearing */
+.process-card-animation-enter-active {
+    transition: all 400ms ease-out;
+}
+
+/* disappearing */
+.process-card-animation-leave-active {
+    transition: all 200ms ease-in;
+    z-index: 0;
+}
+
+/* appear at / disappear to */
+.process-card-animation-enter,
+.process-card-animation-leave-to {
+    opacity: 0;
+
+    div.process-progress-wrapper {
+        opacity: 0;
     }
 }
 
