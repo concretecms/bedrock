a29614dcaf5e56616d27bcbe5f1c67d8a86077c8
Andrew Embler <andrew@concrete5.org>
2021-10-22 08:19:08 -0700

    Fix JS errors in account page

--- assets/account/js/frontend.js
+++ assets/account/js/frontend.js
@@ -2,15 +2,18 @@
 
 import AvatarCropper from './frontend/components/Avatar/Cropper.vue'
 
-window.Concrete.Vue.createContext('frontend', {
-    AvatarCropper
-})
+$(function() {
 
-if (document.querySelectorAll('[data-view=account]').length) {
-    Concrete.Vue.activateContext('frontend', function (Vue, config) {
-        new Vue({
-            el: '[data-view=account]',
-            components: config.components
-        })
+    window.Concrete.Vue.createContext('frontend', {
+        AvatarCropper
     })
-}
+
+    if (document.querySelectorAll('[data-view=account]').length) {
+        Concrete.Vue.activateContext('frontend', function (Vue, config) {
+            new Vue({
+                el: '[data-view=account]',
+                components: config.components
+            })
+        })
+    }
+})
