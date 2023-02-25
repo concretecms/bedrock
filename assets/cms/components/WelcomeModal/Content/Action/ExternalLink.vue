d112a1efff10a113997e018ada613bd5c37b5fc7
Andrew Embler <andrew@concrete5.org>
2023-02-25 06:47:17 -0800

    forgot some files

---
+++ assets/cms/components/WelcomeModal/Content/Action/ExternalLink.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <a target="_blank" :href="url">
+            <i class="fas fa-external-link-alt"></i>
+            Learn More
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    data: () => ({
+    }),
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
