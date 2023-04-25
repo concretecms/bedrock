c964cdf673e803c53f47e4830725fa5369c317ae
deanL-zuiderlicht <49557292+deanL-zuiderlicht@users.noreply.github.com>
2023-04-25 13:24:35 +0200

    Make sure unchecked nodes stay unchecked
    Make sure unchecked child nodes stay unchecked after expanding and collapsing a node.

--- assets/cms/js/tree.js
+++ assets/cms/js/tree.js
@@ -229,6 +229,14 @@ ConcreteTree.prototype = {
                     });
                 }
             },
+            collapse: function(event, data) {
+                //loop over child nodes and check if node is still selected. If not remove it from the 'options.ajaxData.selected' array.
+                data.node.children.forEach(function(nodeChild) {
+                    if (options.ajaxData.selected.includes(nodeChild.key) && !nodeChild.isSelected()) {
+                        delete options.ajaxData.selected.splice(options.ajaxData.selected.indexOf(nodeChild.key), 1);
+                    }
+                });
+            },
             dnd: {
                 preventRecursiveMoves: true, // Prevent dropping nodes on own descendants,
                 focusOnClick: true,
