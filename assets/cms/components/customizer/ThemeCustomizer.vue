38a559292e5f359e0845ab439efc37a95b0e4bc6
Andrew Embler <andrew@concrete5.org>
2025-09-10 08:55:49 -0700

    npm run lint

--- assets/cms/components/customizer/ThemeCustomizer.vue
+++ assets/cms/components/customizer/ThemeCustomizer.vue
@@ -343,7 +343,7 @@ export default {
 
         // Remove the data-dialog=theme-customizer-custom-css div if it appears
         // inside a div with the [role=dialog]. Fixes https://github.com/concretecms/concretecms/issues/12575
-        let $customCssDialog = $('div[role=dialog] div[data-dialog=theme-customizer-custom-css]')
+        const $customCssDialog = $('div[role=dialog] div[data-dialog=theme-customizer-custom-css]')
         if ($customCssDialog.length) {
             $customCssDialog.remove()
         }
