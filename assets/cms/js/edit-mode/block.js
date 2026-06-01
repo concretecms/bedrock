72cf9a58d70441da4fd084a3beac1d9d53e4e644
Jans Carton <jans@websanity.com>
2026-06-01 15:13:21 -0500

    preserve the container notch when moving blocks and keep the drag handle clickable

--- assets/cms/js/edit-mode/block.js
+++ assets/cms/js/edit-mode/block.js
@@ -121,11 +121,12 @@
             var my = this
             var targetDragAreas = targetArea.getDragAreas()
             var myElem
+            var containerWrapper = my.getContainer()
 
             afterBlock = afterBlock || null
             my.getArea().removeBlock(my)
-            my.getContainer().remove()
             if (my.getWraps()) {
+                containerWrapper.remove()
                 myElem = $(targetArea.getBlockTemplate()())
                 if (myElem.children().length) {
                     myElem.find('div.block').replaceWith(my.getElem())
@@ -133,7 +134,7 @@
                     myElem.append(my.getElem())
                 }
             } else {
-                myElem = my.getElem()
+                myElem = containerWrapper.detach()
             }
             if (targetDragAreas.length > 0) {
                 var targetDragArea
