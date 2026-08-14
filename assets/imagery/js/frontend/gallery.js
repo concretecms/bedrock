0b777956fcf58aeafa5cf85014d98cc241103d49
Andrew Embler <andrew@concrete5.org>
2026-08-14 09:45:35 -0700

    Tweaking magnific popup title/caption append

--- assets/imagery/js/frontend/gallery.js
+++ assets/imagery/js/frontend/gallery.js
@@ -1,3 +1,23 @@
+var appendDownloadLink = function (item) {
+    var downloadLink = item && item.el ? item.el.attr('data-download-link') : ''
+    var magnificPopup = $.magnificPopup.instance
+    if (!magnificPopup || !magnificPopup.content) {
+        return
+    }
+
+    var $title = magnificPopup.content.find('.mfp-title')
+    $title.find('[data-gallery-download-link=true]').remove()
+    if (downloadLink && $title.length) {
+        $('<a></a>')
+            .attr('href', downloadLink)
+            .attr('target', '_blank')
+            .attr('class', 'ms-2')
+            .attr('data-gallery-download-link', 'true')
+            .text('Download')
+            .appendTo($title)
+    }
+}
+
 $('[data-gallery-lightbox=true]').magnificPopup({
     type: 'image',
     gallery: {
@@ -5,19 +25,15 @@ $('[data-gallery-lightbox=true]').magnificPopup({
     },
     image: {
         titleSrc: function (item) {
-            var $wrapper = $('<div />')
-            var caption = item.el.attr('data-caption')
-            $wrapper.append(caption)
-            var downloadLink = item.el.attr('data-download-link')
-            if (downloadLink.length) {
-                var $a = $('<a></a>')
-                $a.attr('href', downloadLink)
-                $a.attr('target', '_blank')
-                $a.attr('class', 'ms-2')
-                $a.html('Download')
-                $wrapper.append($a)
-            }
-            return $wrapper.html()
+            return item.el.attr('data-caption') || ''
+        }
+    },
+    callbacks: {
+        open: function () {
+            appendDownloadLink(this.currItem)
+        },
+        change: function () {
+            appendDownloadLink(this.currItem)
         }
     }
 })
